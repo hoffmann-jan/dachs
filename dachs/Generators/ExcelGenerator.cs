d46034771831b37a4c463706a59f95d8803abeb3
jan.mac <jkaneneks@icloud.com>
2018-10-12 13:16:51 +0200

    Update dachs/Generators/ExcelGenerator.cs

--- dachs/Generators/ExcelGenerator.cs
+++ dachs/Generators/ExcelGenerator.cs
@@ -1,4 +1,7 @@
-﻿using dachs.Interfaces;
+﻿using System.Collections.Generic;
+using System.IO;
+using dachs.Interfaces;
+using OfficeOpenXml;
 
 namespace dachs.Generators
 {
@@ -8,7 +11,8 @@ namespace dachs.Generators
     public class ExcelGenerator : IFileGenerator
     {
         #region Fields
-
+        private string _Path;
+        private string _StreetName;
         #endregion
 
         #region Properties
@@ -19,7 +23,12 @@ namespace dachs.Generators
         /// <summary>
         /// Basis-Konstruktor
         /// </summary>
-        public ExcelGenerator() { }
+        public ExcelGenerator(string path, string streetName) 
+        {
+            _Path = path;
+            _StreetName = streetName;
+        }
+
         #endregion
 
         #region Public Methods
@@ -30,11 +39,52 @@ namespace dachs.Generators
 
         #endregion
 
-        #region IFileGenerators
-        void IFileGenerator.Generate()
+        #region IFileGenerator
+        /// <summary>
+        /// Dachs.s the interfaces. IFile generator. generate.
+        /// </summary>
+        /// <param name="content">Content.</param>
+        void IFileGenerator.Generate(IEnumerable<string> content)
         {
-            throw new System.NotImplementedException();
+            ExcelPackage package = new ExcelPackage();
+
+            package.Workbook.Properties.Title = "Street of Le";
+            package.Workbook.Properties.Author = "dachs";
+            package.Workbook.Properties.Subject = _StreetName;
+            package.Workbook.Properties.Keywords = "Leipzig,Straßenname,Hausnummern";
+
+
+            var worksheet = package.Workbook.Worksheets.Add(_StreetName);
+
+            //First add the headers
+            worksheet.Cells[1, 1].Value = "Straßenname";
+            worksheet.Cells[1, 2].Value = "Hausnummer";
+
+            bool first = true;
+            int index = 2;
+
+            foreach(string number in content)
+            {
+                if (first)
+                {
+                    worksheet.Cells[index, 1].Value = _StreetName;
+                    worksheet.Cells[index, 2].Value = number;
+                    first = false;
+                }
+                else
+                {
+                    worksheet.Cells[index, 2].Value = number;
+                }
+
+                index++;
+            }
+
+            using (Stream stream = new FileStream(_Path, FileMode.Create))
+            {
+                package.SaveAs(stream);
+            }
         }
+
         #endregion
     }
 }
