9823f431acacc10fa29e3a9fd0f1eddf788d0f8c
jan.mac <jkaneneks@icloud.com>
2018-10-12 12:49:26 +0200

    Update dachs/Interfaces/IFileGenerator.cs

--- dachs/Interfaces/IFileGenerator.cs
+++ dachs/Interfaces/IFileGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace dachs.Interfaces
 {
     /// <summary>
@@ -6,6 +7,6 @@ namespace dachs.Interfaces
     /// </summary>
     public interface IFileGenerator
     {
-        void Generate();
+        void Generate(IEnumerable<string> content);
     }
 }
